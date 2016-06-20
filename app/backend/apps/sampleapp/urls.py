8f1215eddf2043e15a86d21265d8cd642455efb4
rodrigopavezi <rodrigopavezi@gmail.com>
2016-06-19 23:11:42 -0400

    Changed load politicians task

--- app/backend/apps/sampleapp/urls.py
+++ app/backend/apps/sampleapp/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^politicians/$', views.PoliticianList.as_view(), name='politician-list'),
     url(r'^politicians/(?P<slug>[a-zA-Z\-0-9]+)/?$', views.PoliticianDetail.as_view(), name='politician-detail'),
-    url(r'^politiciansload/$', views.LoadPoliticians.as_view(), name='politician-load'),
 ]
--- app/backend/apps/sampleapp/views.py
+++ app/backend/apps/sampleapp/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import Politician
 from .serializers import PoliticianSerializer
 from rest_framework import generics
-import os
-import csv
 
 
 class PoliticianList(generics.ListCreateAPIView):
@@ -15,21 +13,6 @@ class PoliticianDetail(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'slug'
     queryset = Politician.objects.all()
     serializer_class = PoliticianSerializer
-
-class LoadPoliticians(generics.ListAPIView):
-    module_dir = os.path.dirname(__file__)  # get current directory
-    file_path = os.path.join(module_dir, 'data/politicians.csv')
-    with open(file_path) as f:
-        reader = csv.reader(f)
-        for row in reader:
-            _, created = Politician.objects.get_or_create(
-                last_name=row[0],
-                first_name=row[1],
-                gender=row[2],
-                constituency=row[3],
-                party=row[4],
-                )
-
 
 # Create your views here.
 def index(request):
