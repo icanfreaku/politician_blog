a330aca85b1e0de4b4a8777f443fe827265190a7
rodrigopavezi <rodrigopavezi@gmail.com>
2016-07-14 23:38:11 +0100

    Implemented the rank page

--- app/frontend/src/app/index.js
+++ app/frontend/src/app/index.js
@@ -1,8 +1,20 @@
-angular.module('myapp', ['ngRoute', 'ngResource', 'ngNamedRoute'])
+angular.module('myapp', ['ngRoute', 'ngResource', 'ngNamedRoute', 'dcbImgFallback'])
 .config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider.otherwise({
         redirectTo: '/'
     });
+})
+.controller('HeaderController', function($scope, $location) {
+    
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+})
+.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    };
 });
 require('./sample');
--- app/frontend/src/app/sample/routes/rank/rank-controller.js
+++ app/frontend/src/app/sample/routes/rank/rank-controller.js
@@ -1,5 +1,63 @@
 angular.module('myapp')
-    .controller('RankController', function($scope, Politician) {
-        $scope.politicians = Politician.list();  
-        console.log($scope.politicians);
+    .controller('RankController', function($scope, Politician, orderByFilter, $location) {
+        $scope.currentPage = 0;
+        $scope.pageSize = 10;
+        $scope.rankBy = "-stats.total";
+        $scope.politicians = Politician.list();
+
+        $scope.politicians.$promise.then(function (result) {
+            $scope.politicians = result;
+            buildPagination();
+            $scope.rank();
+            console.log($scope.politicians);
+        });
+
+        var getItems = function () {
+            var items = $scope.politicians;
+            if ($scope.query && $scope.filtered.length > 0) {
+                items = $scope.filtered;
+            }
+            return items;    
+        };
+
+        var buildPagination = function () {
+            
+            $scope.numberOfPages = new Array(Math.ceil(getItems().length/$scope.pageSize));
+        };
+
+        $scope.rank = function () {
+            $scope.politicians = orderByFilter($scope.politicians, $scope.rankBy);
+        };
+
+        $scope.changePage = function (page) {
+            if (page >= 0 && page <= (getItems().length/$scope.pageSize)) {
+                $scope.currentPage = page;
+            }
+        };
+
+        $scope.go = function (politician) {
+            $location.path('/politician/' + politician.slug);     
+        };
+
+        $scope.search = function(item){
+            var found = false;
+            var fullName = item.first_name + " " + item.last_name;
+            if (!$scope.query || (item.first_name.toLowerCase().indexOf($scope.query.toLowerCase()) != -1) || 
+                (item.last_name.toLowerCase().indexOf($scope.query.toLowerCase()) != -1) || 
+                (fullName.toLowerCase().indexOf($scope.query.toLowerCase()) != -1)) {
+
+                found = true;
+            }
+            
+            buildPagination();        
+            
+            return found;
+        };
+
+        $scope.hasNext = function () {
+            return $scope.currentPage >= getItems().length/$scope.pageSize - 1;
+        };
+
+
+
     });
